61aff4d4be7f0d9361a933afc6c1179555bd0839
Eric Sutton <eric.sutton@colorado.edu>
2022-07-27 14:25:32 -0600

    Update x-axis range of HASDM plot

--- hasdm_plot/make_Starlink_HASDM_plot.m
+++ hasdm_plot/make_Starlink_HASDM_plot.m
@@ -6,5 +6,6 @@ title('HASDM, 22^\circ N, 12:45 LT, 210 km');
 xlabel('Day of Year 2022');ylabel('Neutral Density (kg/m^3)');
 set(gca,'XMinorTick','on','YMinorTick','on')
 set(gcf,'PaperPosition',[0.3611    4    7.7778    3])     
+xlim([25,40]);
 ylim([1.7,2.5]*1e-10)
 print(gcf,'-depsc2','StarlinkFeb2022_hasdm');
